12ac5ea2f2810124ac190a6a20a26a35d990a09d
Mohamed Ragab <33547443+mohamedragab33@users.noreply.github.com>
2018-04-16 22:38:22 +0200

    Add files via upload

---
+++ FULL1_APLLICATION/FULL1_APLLICATION/Form1.Designer.cs
@@ -0,0 +1,300 @@
+﻿namespace FULL1_APLLICATION
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.label1 = new System.Windows.Forms.Label();
+            this.searchtxt = new System.Windows.Forms.TextBox();
+            this.searchButton = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.datetxt = new System.Windows.Forms.DateTimePicker();
+            this.authorTXT = new System.Windows.Forms.TextBox();
+            this.nameTXT = new System.Windows.Forms.TextBox();
+            this.IDtxt = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(28, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(236, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Search for book with ID";
+            // 
+            // searchtxt
+            // 
+            this.searchtxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.searchtxt.Location = new System.Drawing.Point(326, 33);
+            this.searchtxt.Name = "searchtxt";
+            this.searchtxt.Size = new System.Drawing.Size(245, 27);
+            this.searchtxt.TabIndex = 5;
+            // 
+            // searchButton
+            // 
+            this.searchButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.searchButton.Location = new System.Drawing.Point(605, 20);
+            this.searchButton.Name = "searchButton";
+            this.searchButton.Size = new System.Drawing.Size(131, 49);
+            this.searchButton.TabIndex = 10;
+            this.searchButton.Text = "show";
+            this.searchButton.UseVisualStyleBackColor = true;
+            this.searchButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(67, 445);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(114, 62);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Insert";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(229, 445);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(136, 62);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "Update";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(405, 445);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(147, 62);
+            this.button3.TabIndex = 14;
+            this.button3.Text = "Delete";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(1353, 523);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(95, 42);
+            this.button4.TabIndex = 15;
+            this.button4.Text = "Close";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(1196, 523);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(108, 42);
+            this.button5.TabIndex = 16;
+            this.button5.Text = "Clear";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.datetxt);
+            this.groupBox1.Controls.Add(this.authorTXT);
+            this.groupBox1.Controls.Add(this.nameTXT);
+            this.groupBox1.Controls.Add(this.IDtxt);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Font = new System.Drawing.Font("Algerian", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(67, 83);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(691, 288);
+            this.groupBox1.TabIndex = 17;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "DATA";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // datetxt
+            // 
+            this.datetxt.CalendarFont = new System.Drawing.Font("Algerian", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.datetxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.datetxt.Location = new System.Drawing.Point(219, 205);
+            this.datetxt.Name = "datetxt";
+            this.datetxt.Size = new System.Drawing.Size(437, 30);
+            this.datetxt.TabIndex = 19;
+            // 
+            // authorTXT
+            // 
+            this.authorTXT.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.authorTXT.Location = new System.Drawing.Point(219, 153);
+            this.authorTXT.Name = "authorTXT";
+            this.authorTXT.Size = new System.Drawing.Size(437, 30);
+            this.authorTXT.TabIndex = 18;
+            // 
+            // nameTXT
+            // 
+            this.nameTXT.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nameTXT.Location = new System.Drawing.Point(219, 101);
+            this.nameTXT.Name = "nameTXT";
+            this.nameTXT.Size = new System.Drawing.Size(437, 30);
+            this.nameTXT.TabIndex = 17;
+            // 
+            // IDtxt
+            // 
+            this.IDtxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.IDtxt.Location = new System.Drawing.Point(219, 54);
+            this.IDtxt.Name = "IDtxt";
+            this.IDtxt.Size = new System.Drawing.Size(437, 30);
+            this.IDtxt.TabIndex = 16;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(16, 207);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(156, 25);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Date of publish";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(16, 155);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(76, 25);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Author";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(16, 101);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 25);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(16, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 25);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "ID";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToOrderColumns = true;
+            dataGridViewCellStyle4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.dataGridView1.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle4;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.LavenderBlush;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(809, 83);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(639, 373);
+            this.dataGridView1.TabIndex = 18;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.searchButton;
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Inherit;
+            this.BackColor = System.Drawing.Color.LavenderBlush;
+            this.ClientSize = new System.Drawing.Size(1473, 589);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.searchButton);
+            this.Controls.Add(this.searchtxt);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox searchtxt;
+        private System.Windows.Forms.Button searchButton;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DateTimePicker datetxt;
+        private System.Windows.Forms.TextBox authorTXT;
+        private System.Windows.Forms.TextBox nameTXT;
+        private System.Windows.Forms.TextBox IDtxt;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ FULL1_APLLICATION/FULL1_APLLICATION/Form1.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace FULL1_APLLICATION
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection cn = new SqlConnection("Server=DESKTOP-C7AP5JN; DataBase=library; Integrated Security =true ; ");
+        SqlCommand cmd;
+        SqlDataAdapter da;
+
+        SqlDataReader dr;
+        DataTable dt = new DataTable();
+        public Form1()
+        {
+            InitializeComponent();
+            da = new SqlDataAdapter("Select * From BOOKS3 ", cn);
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //try
+            //{
+            //    cmd = new SqlCommand("Select ID,NAME,AUTHOR,DATA_PUBLISH From BOOKS3 Where ID ='" + searchtxt + "' ", cn);
+            //    cn.Open();
+            //    dr = cmd.ExecuteReader();
+            //    dr.Read();
+            //    IDtxt.Text = dr["ID"].ToString();
+            //    nameTXT.Text = dr["NAME"].ToString();
+            //    authorTXT.Text = dr["AUTHOR"].ToString();
+            //    datetxt.Text = dr["DATA_PUBLISH"].ToString();
+            //}
+            //catch (Exception ex1)
+            //{
+            //    MessageBox.Show("ERROR MESSAGE ", "ERROR");
+            //}
+            //finally {
+            //    dr.Close();
+            //    cn.Close();
+            //           }
+            try
+            {
+                cmd = new SqlCommand("Select ID, NAME , AUTHOR, DATA_PUBLISH From BOOKS3 Where ID='" + searchtxt.Text + "'; ", cn);
+                cn.Open();
+                dr = cmd.ExecuteReader();
+                dr.Read();
+                IDtxt.Text = dr["ID"].ToString();
+                nameTXT.Text = dr["NAME"].ToString();
+                authorTXT.Text = dr["AUTHOR"].ToString();
+                datetxt.Text = dr["DATA_PUBLISH"].ToString();
+            }
+            catch
+            {
+                MessageBox.Show("ERROR this ID NOT Found please try again", "ERROR");
+            }
+            finally
+            {
+
+                dr.Close();
+                cn.Close();
+            }
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+
+
+            try
+            {
+
+                cmd = new SqlCommand("insert into BOOKS3 (ID,NAME,AUTHOR,DATA_PUBLISH) values ('" + IDtxt.Text + "','" + nameTXT.Text + "','" + authorTXT.Text + "','" + datetxt.Value + "')", cn);
+                cn.Open();
+                cmd.ExecuteNonQuery();
+                da = new SqlDataAdapter("Select ID, NAME , AUTHOR, DATA_PUBLISH From BOOKS3 Where ID='" + IDtxt.Text + "' ", cn);
+                da.Fill(dt);
+                dataGridView1.DataSource = dt;
+
+                searchtxt.Text = "";
+                authorTXT.Text = "";
+                nameTXT.Text = "";
+                IDtxt.Text = "";
+
+
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show("Error" + ex);
+            }
+            finally { cn.Close(); }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            searchtxt.Text = "";
+            authorTXT.Text = "";
+            nameTXT.Text = "";
+            IDtxt.Text = "";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                cmd = new SqlCommand("Delete from BOOKS3 Where ID='" + searchtxt.Text + "'", cn);
+                cn.Open();
+                cmd.ExecuteNonQuery();
+                da = new SqlDataAdapter("Select ID, NAME , AUTHOR, DATA_PUBLISH From BOOKS3 Where ID='" + IDtxt.Text + "' ", cn);
+                da.Fill(dt);
+                dataGridView1.DataSource = dt;
+                searchtxt.Text = "";
+                authorTXT.Text = "";
+                nameTXT.Text = "";
+                IDtxt.Text = "";
+
+            }
+            catch
+            {
+                MessageBox.Show("Massive Error", "Error");
+
+            }
+            finally
+            {
+                cn.Close();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                cmd = new SqlCommand("Update BOOKS3 Set ID='" + IDtxt.Text + "', NAME='" + nameTXT.Text + "', AUTHOR='" + authorTXT.Text + "', DATA_PUBLISH='" + datetxt.Value + "' Where ID='" + searchtxt.Text + "' ", cn);
+                cn.Open();
+                cmd.ExecuteNonQuery();
+                da = new SqlDataAdapter("Select ID, NAME , AUTHOR, DATA_PUBLISH From BOOKS3 Where ID='" + IDtxt.Text + "' ", cn);
+                da.Fill(dt);
+                dataGridView1.DataSource = dt;
+                searchtxt.Text = "";
+                authorTXT.Text = "";
+                nameTXT.Text = "";
+                IDtxt.Text = "";
+            }
+            catch
+            {
+                MessageBox.Show("ERROR", "ERROR");
+            }
+            finally
+            {
+                cn.Close();
+            }
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
